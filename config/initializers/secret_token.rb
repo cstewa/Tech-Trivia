b3065ae018cb72f3b264d1c1df86e8761d041439
Baylee Feore <baylee.feore@gmail.com>
2013-08-02 14:31:24 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizGame::Application.config.secret_token = '88d7f915bf65e07effb2b63ed59bb94f9febfa9969a370779d2d2abc510d2bdedbffd7ec97c436d0e7c8a2d4f762f78990619df6cbe630d687c9d48732fb8a0d'
